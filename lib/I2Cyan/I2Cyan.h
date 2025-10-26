49cdac63414d96584699a5754187251f060d6e11
EnginEar-Proto <pasztorg05@gmail.com>
2025-10-26 17:21:49 +0100

    Custom implemented I2C manager foundation

---
+++ lib/I2Cyan/I2Cyan.h
@@ -0,0 +1,8 @@
+/**
+ * Self-implemented I2C utility classes, following the Adapter pattern.
+ * 
+ * This feature implements
+ * - `I2CAdapter` interface for uniting the useage of the I2C network.
+ * - 
+ * - 
+ */
